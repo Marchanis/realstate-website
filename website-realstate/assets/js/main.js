28e8071b13df17c4248a2089774fdfad1562eb4e
Marchani Salwa <118909843+Marchanis@users.noreply.github.com>
2024-04-27 18:19:55 +0100

    Add files via upload

---
+++ website-realstate/assets/js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+/** display click navbar */
+
+/** {nodeelement} */ 
+
+const $navbar = document.querySelector("[data-navbar]");
+const $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+/** Header scroll state  */
+
+const $header =document.querySelector("[data-header]");
+window.addEventListener("scroll", e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/* add to favorite button toggle */
+
+const $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    });
+});
